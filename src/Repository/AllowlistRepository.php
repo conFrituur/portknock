bff6c8bbfbe4ca1bb199a28eef9e9bf9c290a43e
conFrituur <git@sk91.nl>
2026-05-07 15:55:23 +0200

    Log exception message when allowlist is corrupted

--- src/Repository/AllowlistRepository.php
+++ src/Repository/AllowlistRepository.php
@@ -16,9 +16,9 @@ class AllowlistRepository extends AbstractFileRepository
 
         try {
             $allowlist = Allowlist::fromJson($allowlistEncoded);
-        } catch (Exception) {
+        } catch (Exception $e) {
             // This will probably only occur on first run
-            Log::notice("data/allowlist.json was malformed or empty, starting anew");
+            Log::notice("data/allowlist.json was malformed or empty, starting anew", ['ExceptionMessage' => $e->getMessage()]);
             $allowlist = new Allowlist([]);
         }
 
